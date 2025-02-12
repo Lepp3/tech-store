90e237b8757e70cd4adeb3430bef4738d88555c1
lmilanov <lubaka05@gmail.com>
2025-02-12 21:55:44 +0200

    implemented device schema and model

---
+++ src/models/Device.js
@@ -0,0 +1,66 @@
+import {model, Schema, Types} from 'mongoose';
+
+const deviceSchema = new Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    hardDisk: {
+        type: String,
+        required: true
+    },
+    screenSize: {
+        type: String,
+        required: true
+    },
+    ram: {
+        type: String,
+        required: true
+    },
+    operationSys: {
+        type: String,
+        required: true
+    },
+    cpu: {
+        type: String,
+        required: true
+    },
+    gpu: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    preferredList: [{
+        type: Types.ObjectId,
+        ref: 'User'
+    }],
+    ownerId: {
+        type: Types.ObjectId,
+        ref: 'User'
+    },
+});
+
+
+const Device = model('Device',deviceSchema);
+
+
+export default Device;
