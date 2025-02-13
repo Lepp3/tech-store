a068a9139c697f32c9102a7f9605cdcd5c907ba9
Lepp3 <lubaka05@gmail.com>
2025-02-13 20:34:03 +0200

    implemented dynamic catalog page

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -7,20 +7,26 @@ import { getErrorMessage } from '../utils/getErrorMessage.js';
 
 const deviceController = new Router();
 
-deviceController.get('/catalog', isAuth ,(req,res)=>{
-    res.render('device/catalog');
+deviceController.get('/catalog', async (req,res)=>{
+    try{
+        const devices = await deviceService.getAllDevices();
+        res.render('device/catalog', {devices});
+    }catch(err){
+        res.redirect('/404')
+    }
+    
 });
 
-deviceController.get('/create', (req,res)=>{
+deviceController.get('/create', isAuth , (req,res)=>{
     res.render('device/create');
 });
 
 deviceController.post('/create', isAuth ,async (req,res)=>{
     const deviceData = req.body;
-    const userId = req.user?.id;
+    const userId = req.user.id;
     try{
         await deviceService.createDevice(deviceData,userId);
-        res.redirect('/catalog');
+        res.redirect('/devices/catalog');
     }catch(err){
         const error = getErrorMessage(err);
         res.render('/create', {error: error, deviceData});
--- src/services/deviceService.js
+++ src/services/deviceService.js
@@ -3,6 +3,12 @@ import Device from "../models/Device.js";
 
 
 const deviceService = {
+    getAllDevices(filter = {}){
+        let query = Device.find({});
+        
+        return query;
+       
+    },
     createDevice(data,userId){
         const result = Device.create({...data,
             ownerId: userId,
